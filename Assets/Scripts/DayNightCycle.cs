6d3f134c827dfbf502e16d318c145cfa1ca7a67f
Cafor <formuloratio@gmail.com>
2025-11-11 15:08:50 +0900

    feat: 낮밤 구현

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float time;
+    public float fullDayLength;
+    public float startTime = 0.4f;
+    private float timeRate;
+    public Vector3 noon; // Vector 90 0 0
+
+    [Header("Sun")]
+    public Light sun;
+    public Gradient sunColor;
+    public AnimationCurve sunIntensity;
+
+    [Header("Moon")]
+    public Light moon;
+    public Gradient moonColor;
+    public AnimationCurve moonIntensity;
+
+    [Header("Other Lighting")]
+    public AnimationCurve lightingIntensityMultiplier; //광원관련
+    public AnimationCurve reflectionIntensityMultiplier; //반사관련
+
+    void Start()
+    {
+        timeRate = 1.0f / fullDayLength;
+        time = startTime;
+    }
+
+    void Update()
+    {
+        time = (time + timeRate * Time.deltaTime) % 1.0f;
+
+        UpdateLighting(sun, sunColor, sunIntensity);
+        UpdateLighting(moon, moonColor, moonIntensity);
+
+        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
+        RenderSettings.reflectionIntensity = reflectionIntensityMultiplier.Evaluate(time);
+    }
+
+    void UpdateLighting(Light lightSource, Gradient gradient, AnimationCurve intensityCurve)
+    {
+        float intensity = intensityCurve.Evaluate(time);
+        //정오 시간을 구해주는 각도 계산
+        lightSource.transform.eulerAngles = (time - (lightSource == sun ? 0.25f : 0.75f)) * noon * 4.0f;
+        lightSource.color = gradient.Evaluate(time);
+        lightSource.intensity = intensity;
+
+        GameObject go = lightSource.gameObject;
+        if (lightSource.intensity == 0 && go.activeInHierarchy)
+        {
+            go.SetActive(false);
+        }
+        else if (lightSource.intensity > 0 && !go.activeInHierarchy)
+        { //광원이 하이라키 창에 켜져있지 않고 0보다 크면
+          //광원 켜기
+            go.SetActive(true);
+        }
+    }
+}
